5b1ccca989cdbe9860990c8d10ac4377fe00bcec
Luke <luket8032@hotmail.com>
2023-08-25 13:09:13 -0500

    Create capitalize fn and test

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
+module.exports = capitalize;
---
+++ test.test.js
@@ -0,0 +1,6 @@
+const capitalize = require('./test.js')
+
+test('capitalize', () => {
+    expect(capitalize('hello')).toBe('Hello');
+});
+
